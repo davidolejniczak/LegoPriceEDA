e3fc5502a2b9b4c59d329022ad2be4edab751c77
davidolejniczak <David.olejniczak@icloud.com>
2025-01-05 18:06:08 -0500

    Updated data

--- dataManager.py
+++ dataManager.py
@@ -1,8 +1,11 @@
 import pandas as pd
 
-df1 = pd.read_csv('data/LegoStatsV1.csv')
-df2 = pd.read_csv('data/LegoStatsV3.csv')
+df1 = pd.read_csv('data/LegoStatsMain.csv')
+df2 = pd.read_csv('data/LegoStatsV2.csv')
 
-dfMain = pd.merge(df1, df2, on='set_id', how='inner')
+df1['set_id'] = df1['set_id'].astype(str)
+df2['set_id'] = df2['set_id'].astype(str)
 
-dfMain.to_csv('LegoStatsMain.csv', index=False)
+dfMain = pd.merge(df1, df2, on='set_id',how='left')
+
+dfMain.to_csv('LegoStatsMainV2.csv', index=False)
