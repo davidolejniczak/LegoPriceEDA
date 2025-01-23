2ddc8943a06f665a53619a4038bc4f3ea5901a25
davidolejniczak <David.olejniczak@icloud.com>
2025-01-22 21:17:54 -0500

    Made constructors

--- backend/mainStats.py
+++ backend/mainStats.py
@@ -1,10 +1,29 @@
 import xgboost as xgb
 import pandas as pd
 
+class SetInfo:
+    def __init__(self,LegoLink,BrickEcoLink,Name): 
+        self.LegoLink = LegoLink
+        self.BrickEcoLink = BrickEcoLink
+        self.Name = Name
+        self.Stats = None
+    
+    def setStats(self,stats):
+        self.Stats = stats
+        
+        
+class SetData:
+    def __init__(self,numParts,numFigs,rPrice,numUniqueFigs,setRating,popPred):
+        self.NumParts = numParts
+        self.NumFigs = numFigs
+        self.rPrice = rPrice
+        self.numUniqueFigs = numUniqueFigs
+        self.setRating =setRating
+        self.popPred = popPred
+        
 def importModel(): 
     xgbModel = xgb.Booster()
     xgbModel.load_model(xgb_lego_model.json)
-
 
 def main():
     importModel()
